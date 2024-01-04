60eb7038221e2af81480e3613ff9f92e79d85979
Dev Aggarwal <146590584+DevAggarwal03@users.noreply.github.com>
2024-01-04 23:09:52 +0530

    1st attempt

---
+++ temp.js
@@ -0,0 +1,14 @@
+// let a = document.body.firstChild;
+// let b = a.firstChild.nextSibling;
+// console.log(a.firstChild.nextSibling);
+// console.log(b.previousSibling)
+// console.log(a.parentNode)
+
+let nav = document.body.firstElementChild;
+let gall = nav.firstElementChild.firstElementChild.nextElementSibling;
+console.log(nav)
+console.log(nav.firstElementChild.firstElementChild.nextElementSibling);
+console.log(gall.previousElementSibling)
+
+
+
